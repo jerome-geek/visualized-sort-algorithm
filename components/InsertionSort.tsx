ffb8278e4fbe68c149103bda124dbd109c33c6b1
Jerome <jerome@spinprotocol.io>
2020-05-05 13:04:19 +0900

    Implement Insertion Sort

--- components/InsertionSort.tsx
+++ components/InsertionSort.tsx
@@ -3,17 +3,42 @@ import { range, shuffle } from "lodash";
 
 const getArr = () => shuffle(range(1, 11));
 
+const swap = (arr: number[], a: number, b: number) => {
+  const tmp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = tmp;
+};
+
+const sort = (arr: number[]) => {
+  // https://en.wikipedia.org/wiki/Insertion_sort
+  let i = 1;
+  while (i < arr.length) {
+    let j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      swap(arr, j, j - 1);
+      j = j - 1;
+    }
+    i = i + 1;
+  }
+};
+
 export default () => {
   const [arr, setArr] = useState(getArr());
 
   const handleShuffle = () => setArr(getArr());
+
+  const handleSort = () => {
+    const sortedArr = [...arr];
+    sort(sortedArr);
+    setArr(sortedArr);
+  };
 
   return (
     <div>
       <div className="board">{arr.join(",")}</div>
       <div className="buttonBox">
         <button onClick={handleShuffle}>shuffle</button>
-        <button>sort</button>
+        <button onClick={handleSort}>sort</button>
       </div>
       <style jsx>
         {`
