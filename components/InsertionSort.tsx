d8fbc37132e5e3cafecff763e0d574abdbddd0d6
Jerome <jerome@spinprotocol.io>
2020-05-14 22:24:07 +0900

    Refactor Board Component using Memo

--- components/InsertionSort.tsx
+++ components/InsertionSort.tsx
@@ -1,4 +1,4 @@
-import { useState, SetStateAction, Dispatch } from 'react';
+import { FC, useState, SetStateAction, Dispatch, memo } from 'react';
 import { range, shuffle } from 'lodash';
 import Bar from './Bar';
 import { getX } from '../util/util';
@@ -8,7 +8,7 @@ type TSetIndex = Dispatch<SetStateAction<number>>;
 type TSet = Dispatch<SetStateAction<any>>;
 
 const DURATION = 40;
-const SIZE = 10;
+const SIZE = 30;
 
 const getArr = () => shuffle(range(1, SIZE + 1));
 
@@ -18,9 +18,9 @@ const swap = (arr: number[], a: number, b: number) => {
   arr[b] = tmp;
 };
 
-const delaySet = (value: any, setArr: TSet) =>
+const delaySet = (value: any, set: TSet) =>
   new Promise((resolve) => {
-    setArr(value);
+    set(value);
     setTimeout(resolve, DURATION);
   });
 
@@ -46,7 +46,40 @@ const sort = async (
   }
 };
 
+interface IPropsBoard {
+  arr: number[];
+}
+
+const areArrEqual = (oldProps: IPropsBoard, props: IPropsBoard) => {
+  return oldProps.arr === props.arr;
+};
+
+const Board: FC<IPropsBoard> = (props) => {
+  const { arr } = props;
+  return (
+    <div className="board">
+      {arr.map((value, i) => {
+        console.log('render Bar');
+        return <Bar key={i} value={value} index={i} />;
+      })}
+      <style jsx>{`
+        .board {
+          width: 100%;
+          height: 200px;
+          color: white;
+          background-color: green;
+          font-size: 40px;
+          position: relative;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+const MemorizedBoard = memo(Board, areArrEqual);
+
 export default () => {
+  const [onOff, setOnOff] = useState('on');
   const [arr, setArr] = useState(getArr());
   const [indexI, setIndexI] = useState(1);
   const [indexJ, setIndexJ] = useState(1);
@@ -64,13 +97,11 @@ export default () => {
     setIsRunning(false);
   };
 
+  const handleOnOff = () => setOnOff(onOff === 'on' ? 'off' : 'on');
+
   return (
     <div>
-      <div className="board">
-        {arr.map((value, i) => (
-          <Bar key={i} value={value} index={i} />
-        ))}
-      </div>
+      <MemorizedBoard arr={arr} />
       <div
         className="index i"
         style={{ transform: `translateX(${getX(indexI)}px)` }}
@@ -83,23 +114,14 @@ export default () => {
       >
         j
       </div>
-
       <div className="buttonBox">
+        {<button onClick={handleOnOff}>{onOff}</button>}
         {!isRunning && <button onClick={handleShuffle}>shuffle</button>}
         {!isRunning && <button onClick={handleSort}>sort</button>}
         {isRunning && <div className="running">Running....</div>}
       </div>
-
       <style jsx>
         {`
-          .board {
-            width: 100%;
-            height: 200px;
-            color: white;
-            background-color: green;
-            font-size: 40px;
-            position: relative;
-          }
           .buttonBox {
             width: 100%;
             hegiht: 60px;
